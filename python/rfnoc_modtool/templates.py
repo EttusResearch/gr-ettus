aac959ee3aa24d428f9969b60f29e07eea5b2e25
Nicolas Cuervo <nicolas.cuervo@ettus.com>
2018-05-10 07:01:52 +0200

    rfnocmodtool: update noc_block templates

--- python/rfnoc_modtool/templates.py
+++ python/rfnoc_modtool/templates.py
@@ -571,7 +571,7 @@ module noc_block_${blockname} #(
     // Computer Engine Clock Domain
     .clk(ce_clk), .reset(ce_rst),
     // Control Sink
-    .set_data(set_data), .set_addr(set_addr), .set_stb(set_stb),
+    .set_data(set_data), .set_addr(set_addr), .set_stb(set_stb), set_time(), .set_has_time(),
     .rb_stb(1'b1), .rb_data(rb_data), .rb_addr(rb_addr),
     // Control Source
     .cmdout_tdata(cmdout_tdata), .cmdout_tlast(cmdout_tlast), .cmdout_tvalid(cmdout_tvalid), .cmdout_tready(cmdout_tready),
@@ -609,6 +609,7 @@ module noc_block_${blockname} #(
     .SIMPLE_MODE(1))
   axi_wrapper (
     .clk(ce_clk), .reset(ce_rst),
+    .bus_clk(bus_clk), .bus_rst(bus_rst),
     .clear_tx_seqnum(clear_tx_seqnum),
     .next_dst(next_dst_sid),
     .set_stb(set_stb), .set_addr(set_addr), .set_data(set_data),
