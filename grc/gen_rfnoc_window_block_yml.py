3683cf103557f63ce4b9e71341da76c25a7ff139
Jonathon Pendlum <jonathon.pendlum@ettus.com>
2022-01-05 04:56:57 -0500

    Add RFNoC Window Block support

---
+++ grc/gen_rfnoc_window_block_yml.py
@@ -0,0 +1,158 @@
+"""
+Copyright 2021 Ettus Research, A National Instruments Brand.
+
+This is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3, or (at your option)
+any later version.
+
+This software is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this software; see the file COPYING.  If not, write to
+the Free Software Foundation, Inc., 51 Franklin Street,
+Boston, MA 02110-1301, USA.
+"""
+
+import sys
+
+MAX_NUM_CHANNELS = 16
+
+MAIN_TMPL = """\
+id: ettus_rfnoc_window
+label: RFNoC Window Block
+
+parameters:
+- id: num_chans
+  label: 'Num Channels'
+  dtype: int
+  options: [ ${", ".join([str(n) for n in range(1, max_num_chans+1)])} ]
+- id: block_args
+  label: Block Args
+  dtype: string
+  default: ""
+- id: device_select
+  label: Device Select
+  dtype: int
+  default: -1
+- id: instance_index
+  label: Instance Select
+  dtype: int
+  default: -1
+${coeffs_params}
+inputs:
+- domain: rfnoc
+  dtype: 'sc16'
+  multiplicity: ${'$'}{num_chans}
+
+outputs:
+- domain: rfnoc
+  dtype: 'sc16'
+  multiplicity: ${'$'}{num_chans}
+
+templates:
+  imports: |-
+    import ettus
+    from gnuradio import uhd
+  make: |-
+    ettus.rfnoc_window(
+        self.rfnoc_graph,
+        uhd.device_addr(${'$'}{block_args}),
+        ${'$'}{device_select},
+        ${'$'}{instance_index})
+${init_params}  callbacks:
+${callback_params}
+
+documentation: |-
+  RFNoC Window Block:
+  Applies a user defined window to the input sample stream which is
+  effectively multiplying groups of samples with the provided coefficients.
+
+  Number of Channels:
+  Number of channels / streams to use with the RFNoC Window block. Note,
+  this is defined by the RFNoC Window Block's FPGA build parameters and
+  GNU Radio Companion is not aware of this value. An error will occur at
+  runtime when connecting blocks if the number of channels is too large.
+
+  Coefficient Type:
+  Choice between floating point or integer coefficients.
+  Floating point coefficients must be within range [-1.0, 1.0].
+  Integer coefficients must be within range [-32768, 32767].
+
+  Coefficients:
+  Array of coefficients. Number of elements in the array implicitly sets
+  the window length, and must be less than or equal to the maximum number
+  of coefficients supported by the block.
+
+file_format: 1
+"""
+
+COEFFS_PARAM = """- id: coeffs_type${n}
+  label: 'Ch${n}: Coeffs Type'
+  dtype: enum
+  options: ['float', 'integer']
+  default: 'float'
+  hide: ${'$'}{ 'part' if num_chans > ${n} else 'all' }
+- id: coeffs_float${n}
+  label: 'Ch${n}: Coefficients'
+  dtype: float_vector
+  default: [1.0]
+  hide: ${'$'}{ 'none' if coeffs_type${n} == 'float' and num_chans > ${n} else 'all' }
+- id: coeffs_int${n}
+  label: 'Ch${n}: Coefficients'
+  dtype: int_vector
+  default: [32767]
+  hide: ${'$'}{ 'none' if coeffs_type${n} == 'integer' and num_chans > ${n} else 'all' }
+"""
+
+INIT_PARAM = """    ${'%'} if coeffs_type${n} == "float" and context.get('num_chans')() > ${n}:
+    self.${'$'}{id}.set_coefficients_float(${'$'}{coeffs_float${n}}, ${n})
+    ${'%'} endif
+    ${'%'} if coeffs_type${n} == "integer" and context.get('num_chans')() > ${n}:
+    self.${'$'}{id}.set_coefficients_int(${'$'}{coeffs_int${n}}, ${n})
+    ${'%'} endif
+"""
+
+CALLBACKS_PARAM = """  - |
+    ${'%'} if coeffs_type${n} == "float" and context.get('num_chans')() > ${n}:
+    self.${'$'}{id}.set_coefficients_float(${'$'}{coeffs_float${n}}, ${n})
+    ${'%'} endif
+  - |
+    ${'%'} if coeffs_type${n} == "integer" and context.get('num_chans')() > ${n}:
+    self.${'$'}{id}.set_coefficients_int(${'$'}{coeffs_int${n}}, ${n})
+    ${'%'} endif
+"""
+
+def parse_tmpl(_tmpl, **kwargs):
+    """ Render _tmpl using the kwargs and write to file """
+    from mako.template import Template
+
+    block_template = Template(_tmpl)
+    return str(block_template.render(**kwargs))
+
+if __name__ == '__main__':
+    file = sys.argv[1];
+    coeffs_params = ''.join([
+        parse_tmpl(COEFFS_PARAM, n=n)
+        for n in range(MAX_NUM_CHANNELS)
+    ])
+    init_params = ''.join([
+        parse_tmpl(INIT_PARAM, n=n)
+        for n in range(MAX_NUM_CHANNELS)
+    ])
+    callback_params = ''.join([
+        parse_tmpl(CALLBACKS_PARAM, n=n)
+        for n in range(MAX_NUM_CHANNELS)
+    ])
+    open(file, 'w').write(
+        parse_tmpl(
+            MAIN_TMPL,
+            max_num_chans=MAX_NUM_CHANNELS,
+            coeffs_params=coeffs_params,
+            init_params=init_params,
+            callback_params=callback_params
+        )
+    )
---
+++ include/ettus/rfnoc_window.h
@@ -0,0 +1,83 @@
+/* -*- c++ -*- */
+/*
+ * Copyright 2019 Ettus Research, a National Instruments Brand.
+ * Copyright 2020 Ettus Research, A National Instruments Brand.
+ *
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ *
+ * gr-ettus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with gr-ettus; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifndef INCLUDED_ETTUS_RFNOC_WINDOW_H
+#define INCLUDED_ETTUS_RFNOC_WINDOW_H
+
+#include <ettus/api.h>
+#include <ettus/rfnoc_block.h>
+
+namespace gr {
+namespace ettus {
+
+/*! RFNoC Window Block
+ *
+ * \ingroup uhd_blk
+ */
+class ETTUS_API rfnoc_window : virtual public rfnoc_block
+{
+public:
+    typedef boost::shared_ptr<rfnoc_window> sptr;
+
+    /*!
+     * \param graph Reference to the rfnoc_graph object this block is attached to
+     * \param block_args Additional block arguments
+     * \param device_select Device Selection
+     * \param instance Instance Selection
+     */
+    static sptr make(rfnoc_graph::sptr graph,
+                     const ::uhd::device_addr_t& block_args,
+                     const int device_select,
+                     const int instance);
+
+    /*! Set the Window coefficients
+     *
+     * \param coeffs Vector of Coeffs (float)
+     * \param chan   Channel Index
+     */
+    virtual void set_coefficients(const std::vector<float>& coeffs,
+                                  const size_t chan) = 0;
+
+    /*! Set the Window coefficients
+     *
+     * \param coeffs Vector of Coeffs (int16)
+     * \param chan   Channel Index
+     */
+    virtual void set_coefficients(const std::vector<int16_t>& coeffs,
+                                  const size_t chan) = 0;
+
+    /*! Get the number of Window coefficients
+     *
+     * \param chan   Channel Index
+     */
+    virtual size_t get_max_num_coefficients(const size_t chan) = 0;
+
+    /*! Returns a vector of Window coefficients
+     *
+     * \param chan   Channel Index
+     */
+    virtual std::vector<int16_t> get_coefficients(const size_t chan) = 0;
+};
+
+} // namespace ettus
+} // namespace gr
+
+#endif /* INCLUDED_ETTUS_RFNOC_WINDOW_H */
