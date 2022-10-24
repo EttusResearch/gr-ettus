64c166b584f107b8189222692c05136aa0c2f1be
Martin Braun <martin.braun@ettus.com>
2022-10-24 22:38:14 +0200

    fir filter: Allow building against all UHD versions
    This uses UHD_VERSION to determine if UHD's FIR filter block controller
    has a multi-channel API or not.
    All channel arguments on the GNU Radio block are now zero (from
    previously no default).
    
    When using UHD 4.1 or 4.0, and trying to access an invalid channel
    index, a bespoke warning is printed to the GR logger.
    
    This also reverts e92e957.

--- lib/rfnoc_fir_filter_impl.cc
+++ lib/rfnoc_fir_filter_impl.cc
@@ -23,9 +23,22 @@
 #include "config.h"
 #endif
 
+#include <uhd/version.hpp>
 #include "rfnoc_fir_filter_impl.h"
 #include <volk/volk.h>
 #include <gnuradio/io_signature.h>
+
+#if UHD_VERSION >= 4020000
+#define HAVE_FIR_FILTER_MULTICHAN
+#else
+#define CHECK_CHAN_VALID(chanvar)                                                        \
+    if (chanvar > 0) {                                                                   \
+        GR_LOG_WARN(d_logger,                                                            \
+                    boost::format("Invalid channel index: %d. This version of UHD only " \
+                                  "supports one channel.") %                             \
+                        chanvar);                                                        \
+    }
+#endif
 
 namespace gr {
 namespace ettus {
@@ -51,7 +64,9 @@ rfnoc_fir_filter_impl::~rfnoc_fir_filter_impl() {}
 /******************************************************************************
  * rfnoc_fir_filter API
  *****************************************************************************/
-void rfnoc_fir_filter_impl::set_coefficients(const std::vector<float>& coeffs)
+void rfnoc_fir_filter_impl::set_coefficients(
+    const std::vector<float>& coeffs,
+    const size_t chan = 0)
 {
     std::vector<int16_t> coeffs_int16(coeffs.size());
 
@@ -59,21 +74,39 @@ void rfnoc_fir_filter_impl::set_coefficients(const std::vector<float>& coeffs)
     int16_t* out = &coeffs_int16.front();
 
     volk_32f_s32f_convert_16i(out, in, 32768.0, coeffs.size());
-    set_coefficients(coeffs_int16);
+    set_coefficients(coeffs_int16, chan);
 }
-void rfnoc_fir_filter_impl::set_coefficients(const std::vector<int16_t>& coeffs)
+
+void rfnoc_fir_filter_impl::set_coefficients(
+    const std::vector<int16_t>& coeffs,
+    const size_t chan = 0)
 {
+#ifdef HAVE_FIR_FILTER_MULTICHAN
+    d_fir_filter_ref->set_coefficients(coeffs, chan);
+#else
+    CHECK_CHAN_VALID(chan)
     d_fir_filter_ref->set_coefficients(coeffs);
+#endif
 }
 
-size_t rfnoc_fir_filter_impl::get_max_num_coefficients()
+size_t rfnoc_fir_filter_impl::get_max_num_coefficients(const size_t chan)
 {
+#ifdef HAVE_FIR_FILTER_MULTICHAN
+    return d_fir_filter_ref->get_max_num_coefficients(chan);
+#else
+    CHECK_CHAN_VALID(chan)
     return d_fir_filter_ref->get_max_num_coefficients();
+#endif
 }
 
-std::vector<int16_t> rfnoc_fir_filter_impl::get_coefficients()
+std::vector<int16_t> rfnoc_fir_filter_impl::get_coefficients(const size_t chan)
 {
+#ifdef HAVE_FIR_FILTER_MULTICHAN
+    return d_fir_filter_ref->get_coefficients(chan);
+#else
+    CHECK_CHAN_VALID(chan)
     return d_fir_filter_ref->get_coefficients();
+#endif
 }
 
 
